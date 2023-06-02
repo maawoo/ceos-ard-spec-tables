2e8bc2b4cb6ff09b903c5edc4d21741252ca546c
maawoo <marco.wolsza@uni-jena.de>
2023-06-02 12:50:08 +0200

    [compare] rename variables, add `compare_same_same`

--- cast/compare.py
+++ cast/compare.py
@@ -40,8 +40,6 @@ def by_item_names(cast_1, cast_2, out_dir=None):
     for k in list(df_dict_1.keys()):
         df1 = df_dict_1[k]
         df2 = df_dict_2[k]
-        
-        # set index to column 'item_name'
         df1.set_index('item_name', inplace=True)
         df2.set_index('item_name', inplace=True)
         
@@ -54,27 +52,34 @@ def by_item_names(cast_1, cast_2, out_dir=None):
                                                                         keep_shape=True,
                                                                         keep_equal=True,
                                                                         result_names=(spec1, spec2))
-        compare_main = df1.loc[common_idx, df1.columns[1:]].compare(df2.loc[common_idx, df2.columns[1:]],
+        compare_same_diff = df1.loc[common_idx, df1.columns[1:]].compare(df2.loc[common_idx, df2.columns[1:]],
                                                                     align_axis=0,
                                                                     keep_shape=True,
                                                                     keep_equal=False,
                                                                     result_names=(spec1, spec2))
         
-        if not all(compare_item_col.index == compare_main.index):
+        if not all(compare_item_col.index == compare_same_diff.index):
             raise ValueError("Indices of compare_item_col and compare_main are not the same!")
         
-        # re-insert column 'item' as first column in compare_main
-        compare_main.insert(0, 'item', compare_item_col)
+        # re-insert column 'item' as first column in compare_main and create a copy
+        compare_same_diff.insert(0, 'item', compare_item_col)
+        compare_same_same = copy.deepcopy(compare_same_diff)
         
         # find and drop duplicate row-pairs
-        dups = compare_main.duplicated(subset=compare_main.columns[1:], keep=False)
-        compare_main.drop(compare_main.index[dups], inplace=True)
+        dups = compare_same_diff.duplicated(subset=compare_same_diff.columns[1:], keep=False)
+        compare_same_diff.drop(compare_same_diff.index[dups], inplace=True)
         
         # Reset index so spec is included as a column
-        compare_main.reset_index(level=1, inplace=True)
-        compare_main.rename(columns={"level_1": "spec"}, inplace=True)
+        compare_same_diff.reset_index(level=1, inplace=True)
+        compare_same_diff.rename(columns={"level_1": "spec"}, inplace=True)
         
-        compare_dict[k] = compare_main
+        # keep only identical rows in compare_main_copy and drop all-NaN columns
+        compare_same_same.drop(compare_same_same.index[~dups], inplace=True)
+        compare_same_same.reset_index(level=1, inplace=True)
+        compare_same_same.rename(columns={"level_1": "spec"}, inplace=True)
+        compare_same_same.dropna(axis=1, how='all', inplace=True)
+        
+        compare_dict[k] = compare_same_diff
     
     with open(out_dir.joinpath(f"{spec1}_{spec2}__same_item_names_diff_vals.csv"), 'w') as f:
         i = 0
